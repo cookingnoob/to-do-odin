0f3e207aee550be086d78918e19668d7cd342455
alan <alan@iMac-de-alan.local>
2023-03-13 22:05:58 -0600

    Add layout in HTML elements, created basic style in the css, started to create DOM elements

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -1,4 +1,22 @@
 import './style.css';
 
+const content = document.querySelector('.content');
 
-console.log('hola');
+// const photo = document.createElement('div');
+// photo.classList.add('photo');
+const photo = createDivClass("photo")
+content.appendChild(photo);
+
+function createDivClass(className){
+    let div = document.createElement('div');
+    div.classList.add(className);
+    return div
+}
+
+//clases:
+    //content es la que existe en el html
+    // photo
+    // textC
+    // about
+    // openHours
+    // location
