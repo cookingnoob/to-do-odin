62b71d68d1b019e5d3389cadd184a9d40178d7e0
alan <alan@iMac-de-alan.local>
2023-03-20 12:23:55 -0600

    first commit

---
+++ toDo/src/index.js
@@ -0,0 +1,25 @@
+//los to do's deben tener:
+//title
+//descripcion
+//dueDate
+//prioridad
+//notas
+//checklist
+//estar dentro de un proyecto o listas separadas
+
+//la logica de la aplicacion debe estar separada
+//crear nuevos to dos
+//completar los todo
+//cambiar prioridad
+// manipulacion del DOM
+
+
+//la interfaz del usuario deberia
+//ver todos los proyectos
+//ver todos los to do en cada proyecto, el titulo y su fecha
+//expandir un to do para ver y editar detalles
+//borrar el to do
+
+
+
+console.log('hola')
